263e6e2db3b8374b4b93fe485d05dbf0d2639fab
marilena daquino <marilena.daquino2@unibo.it>
2018-05-28 00:31:10 +0200

    added authors ; moved results in another page ; added export table and buttons

--- bin/app.py
+++ bin/app.py
@@ -1,54 +1,92 @@
-import web , re , urlparse, requests , string , urllib
+import web , re , urlparse, requests , string , urllib , time , datetime
 from web import form
 
 # routing
 urls = (
-	'/','index'
+	'/','index',
+    '/results', 'results'
 )
 
 
 # templates
 render = web.template.render('templates/', base="layout", globals={'re':re})
 
+class DinamycForm(form.Form):
+    """dynamic form"""
+    def add_input(self, new_input):
+        list_inputs = list(self.inputs)
+        list_inputs.insert( (len(list_inputs) -1), new_input)
+        self.inputs = tuple(list_inputs)
 
+authorForm = DinamycForm(
+    form.Textbox('author', form.notnull , description='author'),
+    form.Button("form_action", value='add' , description='add', type="submit")
+    )
+
+# TODO what are the parameters for the citing entity?
 myform = form.Form( 
-	form.Textbox("ORCID",
-		form.notnull,
-		form.regexp(r'^http[s]?:\/\/orcid.org\/(\d{4})-(\d{4})-(\d{4})-(\d{3}[0-9X])$', 'Must be a ORCID')), #regex suggested by pkp (ojs)
-    form.Textbox("DOI", 
-        form.notnull,
+    form.Textbox('title', form.notnull),
+    form.Textbox('journal'),
+    form.Textbox('volume'),
+    form.Textbox('issue'),
+    form.Textbox('year', form.notnull),
+    form.Textbox('publisher', form.notnull),
+	# form.Textbox("ORCID", form.notnull,
+	# 	form.regexp(r'^http[s]?:\/\/orcid.org\/(\d{4})-(\d{4})-(\d{4})-(\d{3}[0-9X])$', 'Must be a ORCID')), #regex suggested by pkp (ojs)
+    form.Textbox("DOI", form.notnull,
         form.regexp(r'^10.\d{4,9}\/[-._;()/:a-zA-Z0-9]+$', 'Must be a DOI')), #regex suggested by crossref
-    form.Textarea('references',
-    	form.notnull),
-    form.Dropdown('style', ['Chicago', 'MLA'], form.notnull)
+    form.Textarea('references', form.notnull , value='paste your references here', post='paste your references here - each paragraph corresponds to a single reference entry.'),
+    form.Dropdown('style', ['Chicago', 'MLA'], form.notnull),
+    form.Button("form_action", value='search' , description='search', type="submit")
     ) 
 
-
-# TO BE CHANGED
-uribaseAPI = ''
-q1 = '?doi='
-q2 = '?style='
-q3 = '?ref='
-
+f = myform() 
+authorform = authorForm()  
 
 class index: 
     def GET(self): 
-        form = myform()
-        return render.index(form, results=None)
+        f = myform()
+        authorform = authorForm()
+        return render.index(authorform, f, results=None)
+
 
     def POST(self): 
-        form = myform() 
-        if not form.validates(): 
-            return render.index(form=form, results=None)
-        else:
-            splitReferencesText = [x for x in string.split(form['references'].value, '\n') if x != '\r'] # extract references from 'references'  
-            results = {}         
-            for referenceText in splitReferencesText:
-            	encodedReference = '+'.join(word for word in re.compile('\w+').findall(referenceText)) 
-            	# request = requests.get(uribaseAPI+q1+form['DOI'].value+q2+form['style'].value+q3+referenceText) 
-            	request = requests.get('https://api.crossref.org/works?query='+encodedReference+'?sample=3&select=DOI,title')
-            	results[referenceText] = request.text
-            return render.index(form=form, results=results)
+        data = web.input()
+        i = web.input(form_action='add')
+        s = web.input(form_action='search')
+        ts = time.time() # TODO change it
+
+        if i.form_action == 'add' and authorform.validates():
+            authorform.add_input(web.form.Textbox('author '+str(len(authorform.inputs)) )) 
+            return render.index(authorForm=authorform, form=f, results=None)
+
+        elif s.form_action == 'search' and authorform.validates():
+            #####################
+            # placeholder for the first call to the API: send metadata of the citing entity: NB multiple authors
+            #####################
+            raise web.seeother('/results?references='+urllib.quote_plus((web.input().references).encode('ascii', 'ignore'))+'?time='+str(ts))
+                              
+
+class results:
+    def GET(self):
+        s = web.input()
+        referencesDecoded = urllib.unquote(web.input().references).decode('utf8')
+        splitReferencesText = [x for x in string.split(referencesDecoded, '\n') if x != '\r'] # extract references from 'references' 
+        results = {}         
+        for referenceText in splitReferencesText:
+            encodedReference = '+'.join(word for word in re.compile('\w+').findall(referenceText))
+            #####################
+            # placeholder for the second call to the API: change the following line
+            #####################
+            request = requests.get('https://api.crossref.org/works?query='+encodedReference+'?sample=1&select=DOI,title')
+            results[referenceText] = request.text
+        sortedResults = sorted(results.items(), key=lambda x: x[0])
+        return render.results(results=sortedResults, content='Placeholder for the citing entity')
+    
+    # def POST(self):
+    #     data = web.input(customRef=[])
+    #     customReferences = data.customRef
+        
 
 
 if __name__ == "__main__":
--- static/js/main.js
+++ static/js/main.js
@@ -1,62 +1,94 @@
 $(document).ready(function () {
 
-  // save editing
-  document.addEventListener('keydown', function (event) {
-  var esc = event.which == 27,
-      nl = event.which == 13,
-      el = event.target,
-      input = el.nodeName != 'INPUT' && el.nodeName != 'TEXTAREA',
-      data = {};
-
-  if (input) {
-    if (esc) {
-      // restore state
-      document.execCommand('undo');
-      el.blur();
-    } else if (nl) {
-      // save
-      data[el.getAttribute('data-name')] = el.innerHTML;
-
-      // we could send an ajax request to update the field
-      
-      $.ajax({
-        url: window.location.toString(),
-        data: data,
-        type: 'post'
-      });
-      
-      log(JSON.stringify(data));
-
-      el.blur();
-      event.preventDefault();
-    }
-  }
-}, true);
-
-
-  // export 2nd column of the table
-  var table = TableExport(document.getElementById("results-table"), {
-    headings: false,                       
-    footers: false, 
-    formats: ["xls", "csv", "txt"],        
-    fileName: "results",                         
-    bootstrap: true,                     
-    position: "bottom",                   
-    ignoreRows: null,                 
-    ignoreCols: [0,2],                 
-    ignoreCSS: ".tableexport-ignore",       
-    emptyCSS: ".tableexport-empty",      
-    trimWhitespace: true          
-  });
-
-
-  // save edits and update table
+  // save edits and update the table
   $(".save").click(function() {
+
+    // export 2nd column of the table
+    var table = TableExport(document.getElementById("results-table"), {
+      headings: false,                       
+      footers: false, 
+      formats: ["xls", "csv", "txt"],        
+      fileName: "results",                         
+      bootstrap: true,                     
+      position: "top",                   
+      ignoreRows: null,                 
+      ignoreCols: [0,2],                 
+      ignoreCSS: ".tableexport-ignore",       
+      emptyCSS: ".tableexport-empty",      
+      trimWhitespace: true          
+    });
+
     var tableUpd = $("#results-table").html();
     $("#results-table").html(tableUpd); 
     table.reset();    
     table.getExportData();
   });
+
+  // style references text area
+  $('#references').attr('rows', '20');
+  $('#references').attr('cols', '120');
+
+  // DOES NOT WORK highlight references in the textarea for double check
+  // $("textarea").markRegExp('(.+?)(\n|$)+');
+
+  // accept / modify buttons
+  $("button.accept").click(function() {
+    var area = $(this).prev('textarea:first').text();
+    var text = $(this).prevAll('textarea:first').html();
+    $(this).prevAll('textarea:first').html(text); 
+    console.log(area);
+    ////////////////////
+    // placeholder to change the call
+    ////////////////////
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://search.crossref.org/dois?q=hallo', true);
+    console.log(request.response);
+    request.send();
+
+    $("button.modify").click(function() {
+      $(this).prevAll('p:first').attr("contenteditable", 'true');
+      var area = $(this).prevAll('p:first').text();
+      console.log(area);
+
+      $('button.accept').click(function() {
+        $(this).prevAll('p:first').attr("contenteditable", 'false');
+        var text = $(this).prevAll('p:first').html();
+        $(this).prevAll('p:first').html(text); 
+        console.log(area);
+        
+        ////////////////////
+        // placeholder to change the call
+        ////////////////////
+        var request = new XMLHttpRequest();
+        request.open('GET', 'http://search.crossref.org/dois?q=hallo', true);
+        console.log(request.response);
+        request.send(); 
+      });
+  });
+    
+  });
+
+  $("button.modify").click(function() {
+      $(this).prevAll('p:first').attr("contenteditable", 'true');
+      var area = $(this).prevAll('p:first').text();
+      console.log(area);
+
+      $('button.accept').click(function() {
+        $(this).prevAll('p:first').attr("contenteditable", 'false');
+        var text = $(this).prevAll('p:first').html();
+        $(this).prevAll('p:first').html(text); 
+        console.log(area);
+        
+        ////////////////////
+        // placeholder to change the call
+        ////////////////////
+        var request = new XMLHttpRequest();
+        request.open('GET', 'http://search.crossref.org/dois?q=hallo', true);
+        console.log(request.response);
+        request.send(); 
+      });
+  });
+
 });
 
 
